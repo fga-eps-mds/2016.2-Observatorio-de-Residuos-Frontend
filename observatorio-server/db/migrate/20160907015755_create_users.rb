370e00930d33304c43196f0796a1061f6113cd29
luizguilherme5 <do.guilherme@hotmail.com>
2016-09-06 23:08:34 -0300

    Generated user model and rake migrate

---
+++ observatorio-server/db/migrate/20160907015755_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.date   :birth_date
+      t.string :city
+      t.string :gender
+      t.string :profile_type
+      t.timestamps null: false
+    end
+  end
+end
