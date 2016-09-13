41075e5d51fdc3eea95c7e8144df834008eeb29d
gustavo2795 <gustavo.unb27@gmail.com>
2016-09-13 11:20:30 -0300

    Signin fix signup

--- observatorio-app/www/js/controllers.js
+++ observatorio-app/www/js/controllers.js
@@ -22,10 +22,22 @@ function ($scope, $stateParams) {
 //   $window.open('/side-menu21/page1', '_self');
 }])
 
-.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, factoryRegister) {
+  $scope.registerEmail= function(user){
+    console.log(user);
+    factoryRegister.save(user, function(result){
+      console.log(result);
+    }, function(error){
+      console.log(error);
+    })
+  }
+
+})
+
+.controller('loginController', function($scope, factoryRegister) {
 
 
-}])
+});
