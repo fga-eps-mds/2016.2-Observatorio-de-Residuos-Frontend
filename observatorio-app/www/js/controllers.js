d64cb6b957e27a4da0276eacb934be9ccf50732d
gustavo2795 <gustavo.unb27@gmail.com>
2016-09-13 15:36:36 -0300

    Fixed home and signout button

--- observatorio-app/www/js/controllers.js
+++ observatorio-app/www/js/controllers.js
@@ -6,6 +6,12 @@ angular.module('app.controllers',[])
 function ($scope, $stateParams) {
 }])
 
+.controller('signoutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+}])
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
