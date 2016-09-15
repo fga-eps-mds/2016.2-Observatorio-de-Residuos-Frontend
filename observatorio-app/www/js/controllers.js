6f3f2264d918860bd9f91bf8960d82e4cc029f76
pablodiegoss <pablodiegoss@hotmail.com>
2016-09-15 10:51:32 -0300

    Fix signup button

--- observatorio-app/www/js/controllers.js
+++ observatorio-app/www/js/controllers.js
@@ -26,13 +26,14 @@ function ($scope, $stateParams) {
 .controller('signupCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, factoryRegister) {
+function ($scope, factoryRegister, $window) {
   $scope.registerEmail= function(isFormValid, user){
     if(isFormValid){
       console.log(user);
       factoryRegister.save(user, function(result){
         console.log(result);
         $scope.errorEmail = false
+        $window.open("#/side-menu/home", "_self")
       }, function(error){
         $scope.errorEmail = true;
       })
