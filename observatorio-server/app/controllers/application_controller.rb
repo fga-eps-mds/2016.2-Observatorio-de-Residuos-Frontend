35dd7632718609746ebe51b5cd1fb60b9deec149
Jefersonalves <jefersonalvesf2009@gmail.com>
2016-09-18 13:55:27 -0300

    Initialize login

--- observatorio-server/app/controllers/application_controller.rb
+++ observatorio-server/app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+<<<<<<< Updated upstream
   before_filter :add_allow_credentials_headers
+=======
+<<<<<<< Updated upstream
+  protect_from_forgery with: :exception
+=======
+  helper_method :current_user
+  before_filter :add_allow_credentials_headers
+
+	def current_user
+	  current_user ||= User.find(session[:user_id]) if session[:user_id] #verifica se está logado
+	end
+
+  def require_user
+	  redirect_to '/login' unless current_user 
+	end
+
+>>>>>>> Stashed changes
   def add_allow_credentials_headers
      response.headers['Access-Control-Allow-Origin'] = '*'
      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
@@ -11,4 +28,8 @@ class ApplicationController < ActionController::Base
    def options
      head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
    end
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 end
--- observatorio-server/app/controllers/users_controller.rb
+++ observatorio-server/app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def new
+    user = User.new
+  end
+
   def create
     user = User.new(user_params)
     if user.save
       render json: user
     else
       render json: { error: 'Incorrect credentials' }, status: 401
+      session[:user_id] = user.id
+	    #redirect_to '/'
+    else
+      render json: { error: 'Incorrect credentials' }, status: 401
+      #redirect_to '/signup'
       puts user.errors.messages
     end
   end
-
 
   private
   def user_params
