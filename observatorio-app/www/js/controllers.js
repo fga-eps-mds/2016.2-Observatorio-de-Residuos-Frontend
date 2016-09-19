160b094a53f9ec96b536f3abee6768ed31cc28ab
pablodiegoss <pablodiegoss@hotmail.com>
2016-09-19 10:29:20 -0300

    fix Login fields

--- observatorio-app/www/js/controllers.js
+++ observatorio-app/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers',[])
+angular.module('app.controllers', [])
 
 .controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -15,13 +15,17 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('signinCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signinCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-// $scope.submit = function () {
-//   $window.open('/side-menu21/page1', '_self');
-}])
+function ($scope, $stateParams, $state) {
+  $scope.loginAttempt = function(isFormValid, user){
+    if(isFormValid){
+      console.log(user);
+      $state.go("menu.home")
+    }
+  }
+})
 
 .controller('signupCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
