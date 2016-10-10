aec5a352d735ef6b1b6914415409b201569f3184
luizguilherme5 <do.guilherme@hotmail.com>
2016-10-09 22:14:02 -0300

    Refactor camera controller

--- observatorio-app/www/js/camera/cameraCtrl.js
+++ observatorio-app/www/js/camera/cameraCtrl.js
@@ -1,16 +1,15 @@
 angular.module('app.controllers')
 
   .controller("cameraCtrl", function ($scope, $cordovaCamera) {
-    /* That is the camera's controller, the following methods are for take a picture or choose one from gallery
-This can be refactored to only had one method, changing the sourceType to ../.CAMERA or ../.PHOTOLIBRARY*/
+    /* That is the camera's controller, the following methods are for take a picture or choose one from gallery*/
 
   $scope.imgURI = "http://placehold.it/800x600";
 
-  $scope.takePhoto = function () {
+  $scope.takePhoto = function (srcType) {
     var options = {
       quality: 75,
       destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
+      sourceType: (srcType=="CAMERA")?Camera.PictureSourceType.CAMERA:Camera.PictureSourceType.PHOTOLIBRARY,
       allowEdit: true,
       encodingType: Camera.EncodingType.JPEG,
       targetWidth: 800,
@@ -22,27 +21,7 @@ This can be refactored to only had one method, changing the sourceType to ../.CA
     $cordovaCamera.getPicture(options).then(function (imageData) {
       $scope.imgURI = "data:image/jpeg;base64," + imageData;
     }, function (err) {
-      console.log("Erro 1 - Camera - Verificar o erro encontrado");
-    });
-  };
-
-  $scope.choosePhoto = function () {
-    var options = {
-      quality: 75,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 800,
-      targetHeight: 600,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false
-    };
-
-    $cordovaCamera.getPicture(options).then(function (imageData) {
-      $scope.imgURI = "data:image/jpeg;base64," + imageData;
-    }, function (err) {
-      console.log("Erro 2 - Camera - Verificar o erro encontrado");
+      console.log("Erro Camera - Verificar o erro encontrado");
     });
   };
 });
