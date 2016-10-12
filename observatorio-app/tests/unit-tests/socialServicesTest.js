198ea6d358256cb729cc9a0a5a65a09d6be15fed
Lucas Amoêdo <lucas_advc@outlook.com>
2016-10-11 22:44:26 -0300

    Update socialServicesTest.js

--- observatorio-app/tests/unit-tests/socialServicesTest.js
+++ observatorio-app/tests/unit-tests/socialServicesTest.js
@@ -1,50 +1,57 @@
 describe('SocialServices', function(){
-  var serviceGoogle
-  var serviceFacebook
-  var userExtractorService
-  var $state
-  beforeEach(module('starter'))
-  beforeEach(inject(function(_googleExtractor_,_facebookExtractor_, _userDataExtractorService_){
-    serviceGoogle = _googleExtractor_
-    serviceFacebook = _facebookExtractor_
-    userExtractorService = _userDataExtractorService_
-    })
-  )
-  describe('googleExtractor', function(){
-    var authData = {google:{email:'amoedo@gmail.com', cachedUserProfile:{given_name:"Amoedo",family_name:'Mito',gender:'male'}}}
-    it('extracts google information from authData object', function(){
-      var userData = serviceGoogle.extract(authData)
-      expect(userData.first_name).toBeDefined()
-      expect(userData.last_name).toBeDefined()
-      expect(userData.gender).toBeDefined()
-      expect(userData.email).toBeDefined()
-    })
-  })
+  var serviceGoogle;
+  var serviceFacebook;
+  var userExtractorService;
+  var $state;
 
-  describe('facebookExtractor', function(){
-    var authData = {facebook:{email:'amoedo@gmail.com', cachedUserProfile:{first_name:"Amoedo",last_name:'Mito',gender:'male'}}}
-    it('extracts facebook information from authData object', function(){
-      var userData = serviceFacebook.extract(authData)
-      expect(userData.first_name).toBeDefined()
-      expect(userData.last_name).toBeDefined()
-      expect(userData.gender).toBeDefined()
-      expect(userData.email).toBeDefined()
-    })
-  })
+  beforeEach(module('starter'));
+
+  beforeEach(inject(function(_googleExtractor_,_facebookExtractor_, _userDataExtractorService_){
+    serviceGoogle = _googleExtractor_;
+    serviceFacebook = _facebookExtractor_;
+    userExtractorService = _userDataExtractorService_;
+    }));
+
+  describe('Extractors', function() {
+    var userData;
+
+    afterEach(function() {
+        expect(userData.first_name).toBe('Amoedo');
+        expect(userData.last_name).toBe('Mito');
+        expect(userData.gender).toBe('male');
+        expect(userData.email).toBe('amoedo@gmail.com');
+    });
+
+    describe('googleExtractor', function(){
+      var authData = {google:{email:'amoedo@gmail.com', cachedUserProfile:{given_name:"Amoedo",family_name:'Mito',gender:'male'}}}
+      it('extracts google information from authData object', function(){
+        userData = serviceGoogle.extract(authData);
+      });
+    });
+
+    describe('facebookExtractor', function(){
+      var authData = {facebook:{email:'amoedo@gmail.com', cachedUserProfile:{first_name:"Amoedo",last_name:'Mito',gender:'male'}}}
+      it('extracts facebook information from authData object', function(){
+        userData = serviceFacebook.extract(authData);
+      });
+    });
+
+  });
 
   describe('userDataExtractorService', function(){
-    var authDataFacebook = {facebook:{email:'amoedo@gmail.com', cachedUserProfile:{first_name:"Amoedo",last_name:'Mito',gender:'male'}}}
-    var authDataGoogle = {google:{email:'amoedo@gmail.com', cachedUserProfile:{given_name:"Amoedo",family_name:'Mito',gender:'male'}}}
+    
     it('calls facebookExtractor with button facebook string parameter', function(){
-        spyOn(serviceFacebook, 'extract')
-        userExtractorService.extract(authDataFacebook,'facebook')
-        expect(serviceFacebook.extract).toHaveBeenCalledWith(authDataFacebook)
-    })
+        var authDataFacebook = {facebook:{email:'amoedo@gmail.com', cachedUserProfile:{first_name:"Amoedo",last_name:'Mito',gender:'male'}}};
+        spyOn(serviceFacebook, 'extract');
+        userExtractorService.extract(authDataFacebook,'facebook');
+        expect(serviceFacebook.extract).toHaveBeenCalledWith(authDataFacebook);
+    });
 
     it('calls googleExtractor with button facebook string parameter', function(){
-        spyOn(serviceGoogle,'extract')
-        userExtractorService.extract(authDataGoogle,'google')
-        expect(serviceGoogle.extract).toHaveBeenCalledWith(authDataGoogle)
-    })
-  })
-})
+        var authDataGoogle = {google:{email:'amoedo@gmail.com', cachedUserProfile:{given_name:"Amoedo",family_name:'Mito',gender:'male'}}};
+        spyOn(serviceGoogle,'extract');
+        userExtractorService.extract(authDataGoogle,'google');
+        expect(serviceGoogle.extract).toHaveBeenCalledWith(authDataGoogle);
+    });
+  });
+});
