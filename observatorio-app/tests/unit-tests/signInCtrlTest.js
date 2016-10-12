34c056ce33a59821e938e4c94e0f10af8f2f1cd3
Lucas Amoêdo <lucas_advc@outlook.com>
2016-10-12 11:10:26 -0300

    Update signInCtrlTest to use module URL constant

--- observatorio-app/tests/unit-tests/signInCtrlTest.js
+++ observatorio-app/tests/unit-tests/signInCtrlTest.js
@@ -1,49 +1,50 @@
 describe('signinCtrl',function(){
 
-    var $controller;
-    var $httpBackend;
-    var $state;
-    var $scope = {};
+  var $controller;
+  var $httpBackend;
+  var $state;
+  var $scope = {};
+  var URL;
 
-    beforeEach(module('starter'));
+  beforeEach(module('starter'));
 
-    beforeEach(inject(function(_$controller_, _$httpBackend_, _$state_) {
-        $controller = _$controller_;
-        $httpBackend = _$httpBackend_;
-        $httpBackend.when('GET', /\.html$/).respond('');
-        $state = _$state_;
-    }));
+  beforeEach(inject(function(_$controller_, _$httpBackend_, _$state_, $injector) {
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
+    $httpBackend.when('GET', /\.html$/).respond('');
+    $state = _$state_;
+    URL = $injector.get('URL');
+  }));
 
-    beforeEach(function() {
-        var controller = $controller('signinCtrl', {$scope: $scope});
-        spyOn($state, 'go');
+  beforeEach(function() {
+    var controller = $controller('signinCtrl', {$scope: $scope});
+    spyOn($state, 'go');
+  });
+
+  describe('loginAttempt',function() {
+    var user = {name:'Amoedo', email:'amoedo@gmail.com'};
+
+    it('login a valid user succesfully', function() {
+      $httpBackend.expectPOST(URL + '/sessions/login', user).respond(201);
+      $scope.loginAttempt(user);
+      $httpBackend.flush();
+      expect($state.go).toHaveBeenCalledWith("menu.home");
+      expect($scope.loginError).toBeFalsy();
     });
 
-    describe('loginAttempt',function() {
-        var URL = "http://localhost:3000/sessions/login";
-        var user = {name:'Amoedo', email:'amoedo@gmail.com'};
-
-        it('login a valid user succesfully', function() {
-            $httpBackend.expectPOST(URL,user).respond(201);
-            $scope.loginAttempt(user);
-            $httpBackend.flush();
-            expect($state.go).toHaveBeenCalledWith("menu.home");
-            expect($scope.loginError).toBeFalsy();
-        });
-
-        it('does not login an invalid user', function() {
-            $httpBackend.expectPOST(URL,user).respond(401);
-            $scope.loginAttempt(user);
-            $httpBackend.flush();
-            expect($state.go).not.toHaveBeenCalled();
-            expect($scope.loginError).toBeTruthy();
-        });
+    it('does not login an invalid user', function() {
+      $httpBackend.expectPOST(URL + '/sessions/login', user).respond(401);
+      $scope.loginAttempt(user);
+      $httpBackend.flush();
+      expect($state.go).not.toHaveBeenCalled();
+      expect($scope.loginError).toBeTruthy();
     });
+  });
 
-    describe('registerSocial', function() {
-        it('logs a user through facebook', function(){
-            $scope.registerSocial('facebook');
-            $httpBackend.flush();
-        });
+  describe('registerSocial', function() {
+    it('logs a user through facebook', function(){
+      $scope.registerSocial('facebook');
+      $httpBackend.flush();
     });
+  });
 });
