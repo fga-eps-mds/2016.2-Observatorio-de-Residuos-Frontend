aa35f9bbb2970c0b1bee1c32c2043f9d936f3dd6
caue96 <cauemateus@hotmail.com>
2016-10-21 01:11:21 -0200

    add new alert error location and improve popup messages

--- www/js/marking/newMarkingCtrl.js
+++ www/js/marking/newMarkingCtrl.js
@@ -1,47 +1,60 @@
 angular.module('app.controllers')
 
-    .controller("newMarkingCtrl", function ($ionicHistory, $scope, $rootScope, $http, factoryMarking, $ionicPopup, $cordovaGeolocation, URL) {
-      $rootScope.markings = [];
+.controller("newMarkingCtrl", function ($ionicHistory, $scope, $rootScope, $http, factoryMarking, $ionicPopup, $cordovaGeolocation, URL) {
+  $rootScope.markings = [];
 
-      $http.get(URL + '/markings')
+  var options = {enableHighAccuracy: true};
 
-          .success(function(content){
-            angular.forEach(content, function(value, key) {
-              $rootScope.markings.push(value);
-            })
-          }).error(function(data){
-        console.log(data)
+  $http.get(URL + '/markings')
+
+    .success(function(content){
+      angular.forEach(content, function(value, key) {
+        $rootScope.markings.push(value);
       })
-
-      $scope.registerMarking = function (marking) {
-        factoryMarking.save(marking, function (result){
-          $rootScope.markings.push({
-            name: marking.name,
-            fire: marking.fire,
-            water: marking.water,
-            earth: marking.earth,
-            air: marking.air,
-            comment: marking.comment,
-            latitude: marking.latitude,
-            longitude: marking.longitude
-          });
-          var alertPopup = $ionicPopup.alert({
-            title: 'Incidente cadastrado com sucesso',
-            template: 'Obrigado por contribuir!'
-          })
-          $scope.marking = {}
-          console.log("Success!")
-          $ionicHistory.nextViewOptions({
-            disableBack: true
-          })
-
-          console.log(marking)
-        }, function (error) {
-          var alertPopup = $ionicPopup.alert({
-            title: 'Informações insuficientes',
-            template: 'Preencha as informções corretamente!'
-          })
-        })
-      }
-
+    }).error(function(data){
+      console.log(data)
     })
+
+  $scope.registerMarking = function (marking) {
+    navigator.geolocation.getCurrentPosition(function(pos){
+      // $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+      marking.latitude = pos.coords.latitude;
+      marking.longitude = pos.coords.longitude;
+
+      console.log(marking)
+
+      factoryMarking.save(marking, function (result){
+        $rootScope.markings.push({
+          name: marking.name,
+          fire: marking.fire,
+          water: marking.water,
+          earth: marking.earth,
+          air: marking.air,
+          description: marking.description,
+          latitude: marking.latitude,
+          longitude: marking.longitude
+        });
+      var alertPopup = $ionicPopup.alert({
+        title: 'Incidente cadastrado com sucesso',
+        template: 'Obrigado por contribuir!'
+      })
+      $scope.marking = {}
+        console.log("Success!")
+        $ionicHistory.nextViewOptions({
+          disableBack: true
+        })
+      console.log(pev)
+      /* This state must be reset and the back button too */
+      }, function (error) {
+        var alertPopup = $ionicPopup.alert({
+        title: 'Informações insuficientes',
+        template: 'Preencha as informações corretamente!'
+        })
+      })
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+      }
+    , options);
+  }
+
+})
