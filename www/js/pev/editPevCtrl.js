237379b27feb0d5ef12ffa767d9efb4995b135dd
pablodiegoss <pablodiegoss@hotmail.com>
2016-10-21 21:53:26 -0200

    create mock method getone pev for pev edit

--- www/js/pev/editPevCtrl.js
+++ www/js/pev/editPevCtrl.js
@@ -1,5 +1,14 @@
 angular.module("app.controllers")
 
-.controller("editPevCtrl", function(){
+.controller("editPevCtrl", function($scope, $http, URL){
+    //$scope.pev = {}
+    $http.get(URL+"/pevs/getonepev")
+    .success(function(pev){
+      console.log(pev);
+      $scope.pev = pev  ;
+      //$scope.pev = result;
+    })
+    .error(function(result){
 
+    })
 })
