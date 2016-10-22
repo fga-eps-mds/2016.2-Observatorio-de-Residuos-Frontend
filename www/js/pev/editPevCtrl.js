1eb8e1eead347322606a8963c0388370d7a574a6
pablodiegoss <pablodiegoss@hotmail.com>
2016-10-21 22:00:47 -0200

    remove getonepev comments

--- www/js/pev/editPevCtrl.js
+++ www/js/pev/editPevCtrl.js
@@ -1,14 +1,12 @@
 angular.module("app.controllers")
 
 .controller("editPevCtrl", function($scope, $http, URL){
-    //$scope.pev = {}
     $http.get(URL+"/pevs/getonepev")
     .success(function(pev){
       console.log(pev);
-      $scope.pev = pev  ;
-      //$scope.pev = result;
+      $scope.pev = pev;
     })
     .error(function(result){
-
+      console.log("ERRROOOWWW");
     })
 })
