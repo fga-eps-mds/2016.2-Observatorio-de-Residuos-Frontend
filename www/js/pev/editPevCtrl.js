7ae4e09e18df73c7a33bfa4c943f80ae2422848e
Flavio Costa <Skingk.f@gmail.com>
2016-10-22 00:20:24 -0200

    Create edit pev function

--- www/js/pev/editPevCtrl.js
+++ www/js/pev/editPevCtrl.js
@@ -1,12 +1,32 @@
 angular.module("app.controllers")
 
-.controller("editPevCtrl", function($scope, $http, URL){
+.controller("editPevCtrl", function($scope, $http, factoryEditPEV, $state, $rootScope, $ionicPopup, URL){
+    var index;
     $http.get(URL+"/pevs/getonepev")
     .success(function(pev){
       console.log(pev);
       $scope.pev = pev;
+      // index = $rootScope.pevs.indexOf(pev);   
     })
+
     .error(function(result){
-      console.log("ERRROOOWWW");
-    })
+      console.log("error");
+    });  
+
+    $scope.editPEV = function(pev){
+   //  	console.log($rootScope.pevs[index]);
+	 	// $rootScope.pevs[index] = pev;
+
+    	factoryEditPEV.save(pev, function(result){
+    		var alertPopup = $ionicPopup.alert({
+              title: 'PEV editada com sucesso',
+              template: 'Obrigado por contribuir!'
+            });
+
+    		$state.go("tabs.map")
+
+		}, function(erro){
+			console.log(erro);
+		})
+    }
 })
