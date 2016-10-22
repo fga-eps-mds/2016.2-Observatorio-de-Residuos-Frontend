7a57f9f14f8f06bc3c6cb543cee17b56580ad005
Jefersonalves <jefersonalvesf2009@gmail.com>
2016-10-22 20:53:53 -0200

    Add popup to markings Pev

--- www/js/map/mapCtrl.js
+++ www/js/map/mapCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers')
 
-.controller('mapCtrl', function(NgMap, $cordovaGeolocation, $scope, $ionicModal) {
+.controller('mapCtrl', function(NgMap, $cordovaGeolocation, $scope, $ionicModal, $ionicPopup) {
   NgMap.getMap().then(function(map) {
     google.maps.event.addListener(map, "rightclick", function(event) {
       $scope.pev = {};
@@ -16,5 +16,19 @@ angular.module('app.controllers')
     $scope.modal = modal;
     });
 
+    $scope.informationPEV = function (pev) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Titulo',
+          template: 'descricao!'
+          //Adicionar pev.name, pev.description e materias recolhidos
+        });
+    }
+/* 
+    $ionicPopover.fromTemplateUrl('views/pev/popover.html', {
+        scope: $scope
+      }).then(function(popover) {
+        $scope.popover = popover;
+      });
+*/
   })
 })
