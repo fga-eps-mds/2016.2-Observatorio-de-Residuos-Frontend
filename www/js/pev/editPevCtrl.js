706c0deb2c8252002ea2550a76f8c346ecb3e0de
pablodiegoss <pablodiegoss@hotmail.com>
2016-10-24 16:07:57 -0200

    edit pev on map as soon as confirm edit

--- www/js/pev/editPevCtrl.js
+++ www/js/pev/editPevCtrl.js
@@ -1,17 +1,17 @@
 angular.module("app.controllers")
 
-.controller("editPevCtrl", function($scope, $http, factoryEditPEV, $state, $rootScope, $ionicPopup, URL){
+.controller("editPevCtrl", function($scope, $http, factoryEditPEV, $state, $rootScope, $ionicPopup,findPevService, URL){
     var index;
     $http.get(URL+"/pevs/getonepev")
     .success(function(pev){
       console.log(pev);
       $scope.pev = pev;
-      // index = $rootScope.pevs.indexOf(pev);   
+      findPevService.findIndex(pev);
     })
 
     .error(function(result){
       console.log("error");
-    });  
+    });
 
 	$scope.confirmEditPEV = function(pev) {
 		var confirmPopup = $ionicPopup.confirm({
@@ -19,25 +19,19 @@ angular.module("app.controllers")
 			template: 'Deseja finalizar a edição desta PEV?'
 		});
 
-		confirmPopup.then(function(res) {
+    confirmPopup.then(function(res) {
 			if(res) {
-			console.log('sim');
-				// $scope.editPEV = function(pev){
-					// console.log($rootScope.pevs[index]);
-					// $rootScope.pevs[index] = pev;
-
-					factoryEditPEV.save(pev, function(result){
+			    factoryEditPEV.save(pev, function(result){
+            index = findPevService.getIndex();
+            $rootScope.pevs[index] = pev;
 						var alertPopup = $ionicPopup.alert({
-						title: 'PEV editada com sucesso',
-						template: 'Obrigado por contribuir!'
+  						title: 'PEV editada com sucesso',
+  						template: 'Obrigado por contribuir!'
 						});
-
-					$state.go("tabs.map")
-
+            $state.go("tabs.map")
 					}, function(erro){
 						console.log(erro);
 					})
-				// }
 			} else {
 			console.log('não');
 			}
