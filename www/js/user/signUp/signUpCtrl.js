95c10d2e8a13e2f2fd67d6956eaa23af5a6f07c0
gustavo2795 <gustavo.unb27@gmail.com>
2016-10-28 18:36:20 -0200

    Add dynamic Perfil

--- www/js/user/signUp/signUpCtrl.js
+++ www/js/user/signUp/signUpCtrl.js
@@ -3,10 +3,21 @@ angular.module('app.controllers')
   registerEmail: Chamado pelo botão da signup.html, irá receber os dados do usuário e salva-los.
                  Usuário recebido aqui já deve ter sido validado pela view.
 */
-.controller('signupCtrl', function ($scope, factoryRegister, currentUserService, $state) {
+.controller('signupCtrl', function ($scope, $http, URL, factoryRegister, currentUserService, $state) {
 
   /*Caso utilizem o botão de login social sem se cadastrar os dados do
     cadastro se preenchem sozinhos através da service de usuário atual.*/
+  $scope.perfis = [];
+
+  $http.get(URL + 'perfis/index')
+  .success(function(perfis){
+    angular.forEach(content, function(value, key) {
+      $scope.perfis.push(value);
+    })
+  })
+  .error(function(error){
+    console.log("Error");
+  })
 
   $scope.user = currentUserService.getUserData();
   console.log($scope.user);
