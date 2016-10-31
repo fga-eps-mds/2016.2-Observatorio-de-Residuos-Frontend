0d5d571b8a2001813d2f29856976bd855a3ebc42
antoniocoj <_antonio_@hotmail.com>
2016-10-31 15:24:28 -0200

    Showing type marking

--- www/js/marking/showMarkingCtrl.js
+++ www/js/marking/showMarkingCtrl.js
@@ -4,11 +4,22 @@ angular.module('starter')
 	$scope.showPev = function(event, pev){
 		$scope.marking = pev;
 		console.log($scope);
+		$scope.types = [];
+		if (pev.paper == true)
+			$scope.types.push("Papel");
+		if (pev.glass == true)
+			$scope.types.push("Vidro");
+		if (pev.metal == true)
+			$scope.types.push("Metal");
+		if (pev.plastic == true)
+			$scope.types.push("Plástico");
+		console.log(pev)
+		console.log($scope.types)
 		$scope.modal.show();
 	};
-		$ionicModal.fromTemplateUrl('views/marking/showMarking.html', {
-			scope: $scope,
-		}).then(function(modal){
-			$scope.modal = modal;
-		});
+	$ionicModal.fromTemplateUrl('views/marking/showMarking.html', {
+		scope: $scope,
+	}).then(function(modal){
+		$scope.modal = modal;
+	});
 });
