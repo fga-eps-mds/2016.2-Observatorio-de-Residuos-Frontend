7425fc79c8a5626bee5d3c377ef58787216a3ff5
antoniocoj <_antonio_@hotmail.com>
2016-10-31 15:32:00 -0200

    Showing incident

--- www/js/marking/showMarkingCtrl.js
+++ www/js/marking/showMarkingCtrl.js
@@ -3,7 +3,6 @@ angular.module('starter')
 .controller('showMarkingCtrl',function($scope, $ionicModal){
 	$scope.showPev = function(event, pev){
 		$scope.marking = pev;
-		console.log($scope);
 		$scope.types = [];
 		if (pev.paper == true)
 			$scope.types.push("Papel");
@@ -13,10 +12,14 @@ angular.module('starter')
 			$scope.types.push("Metal");
 		if (pev.plastic == true)
 			$scope.types.push("Plástico");
-		console.log(pev)
-		console.log($scope.types)
 		$scope.modal.show();
 	};
+	$scope.showIncident = function(event, incident){
+		$scope.marking = incident;
+		$scope.types = [];
+		$scope.modal.show();
+	};
+
 	$ionicModal.fromTemplateUrl('views/marking/showMarking.html', {
 		scope: $scope,
 	}).then(function(modal){
