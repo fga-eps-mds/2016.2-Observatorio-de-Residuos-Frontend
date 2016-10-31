d46752348194b28442177b5cda022c4db5f50002
antoniocoj <_antonio_@hotmail.com>
2016-10-31 14:08:24 -0200

    Creating marking modal

--- www/js/marking/showMarkingCtrl.js
+++ www/js/marking/showMarkingCtrl.js
@@ -1,9 +1,13 @@
 angular.module('starter')
 
-.controller('showMarkingCtrl',function($scope){
+.controller('showMarkingCtrl',function($scope, $ionicModal){
 	$scope.showPev = function(){
 		console.log('PEV clicada.');
-		
+		$scope.modal.show();
 	};
-
+		$ionicModal.fromTemplateUrl('views/marking/showMarking.html', {
+			scope: $scope,
+		}).then(function(modal){
+			$scope.modal = modal;
+		});
 });
