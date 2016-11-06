14faf307a895430767479ac3109a0addb3e9c39d
pablodiegoss <pablodiegoss@hotmail.com>
2016-11-06 19:13:16 -0200

    fix state change

--- www/js/user/profile/editProfileCtrl.js
+++ www/js/user/profile/editProfileCtrl.js
@@ -1,11 +1,11 @@
 angular.module('app.controllers')
 
-    .controller('editProfileCtrl', function($scope, currentUserService, factoryProfile) {
+    .controller('editProfileCtrl', function($scope, currentUserService, factoryProfile, $state) {
         $scope.user = currentUserService.getUserData()
         $scope.editUser = function (user) {
             factoryProfile.save(user, function (result) {
                 currentUserService.setUserData(user)
-                state.go('profile')
+                $state.go('profile')
             }, function (error) {
                 console.log(error)
             })
