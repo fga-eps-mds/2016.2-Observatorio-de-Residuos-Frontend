7299912f9364d66cea0c0f857f9ad4d2ac3eab9f
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-06 18:08:46 -0200

    Add current Marking Service

---
+++ www/js/marking/currentMarkingService.js
@@ -0,0 +1,15 @@
+angular.module("starter")
+
+.service("currentMarkingService", function(){
+	var marking = "";
+	var getMarking = function(){
+		return marking;
+	}
+	var setMarking = function(paramMarking){
+		marking = paramMarking;
+	}
+	return{
+		getMarking: getMarking,
+		setMarking: setMarking
+	}
+})
