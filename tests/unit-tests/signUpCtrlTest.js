dc983b48ee4276e5127394a707149a250a7af71e
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-08 14:58:36 -0200

    Fix caca dos merges nos tests

--- tests/unit-tests/signUpCtrlTest.js
+++ tests/unit-tests/signUpCtrlTest.js
@@ -49,7 +49,7 @@ describe('signupCtrl', function() {
       spyOn($state, 'go');
       $httpBackend.flush();
       expect($scope.invalidEmail).toBeFalsy();
-      expect($state.go).toHaveBeenCalledWith('tutorial');//------
+      expect($state.go).toHaveBeenCalledWith('home');//------
     });
 
     it('should set invalidEmail to true when getting an error response from\
