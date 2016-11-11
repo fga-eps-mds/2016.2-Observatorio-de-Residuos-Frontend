754ae9b5bce8dd76d9b09f6fa79b498cb04fa90e
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-11 20:18:35 -0200

    Add profile to edit profile

--- www/js/user/profile/editProfileCtrl.js
+++ www/js/user/profile/editProfileCtrl.js
@@ -1,6 +1,20 @@
 angular.module('app.controllers')
 
-    .controller('editProfileCtrl', function($scope, currentUserService, factoryProfile, $state) {
+    .controller('editProfileCtrl', function($scope, $http, URL, $rootScope, currentUserService, factoryProfile, $state) {
+      $rootScope.profiles = [];
+
+      $http.get(URL + '/profiles')
+      .success(function(content){
+        angular.forEach(content, function(value, key) {
+          $rootScope.profiles.push(value);
+
+        })
+        console.log($rootScope.profiles);
+      })
+      .error(function(error){
+        console.log("Error");
+      })
+
         $scope.user = currentUserService.getUserData()
         $scope.editUser = function (user) {
             factoryProfile.save(user, function (result) {
