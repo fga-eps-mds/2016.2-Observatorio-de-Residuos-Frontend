1605279bed4a5e4eede5ad324d1cb23ef0f0d001
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-11 10:53:37 -0200

    Add comment in showMakingCtrl

--- www/js/marking/showMarkingCtrl.js
+++ www/js/marking/showMarkingCtrl.js
@@ -1,7 +1,8 @@
 angular.module('starter')
-
+// Controller da modal que mostra as informações da marcação clicada
 .controller('showMarkingCtrl',function($scope, $rootScope, currentMarkingService, $ionicModal, currentUserService, $state){
 	var currentMarking = "";
+	//Função que coloca como escopo as informações da PEV clicada.
 	$scope.showPev = function(event, pev){
 		$scope.currentUserEmail = currentUserService.getUserData().email;
 		console.log(pev)
@@ -17,6 +18,7 @@ angular.module('starter')
 			$scope.types.push("Plástico");
 		$scope.modal.show();
 	};
+	//Função que coloca como escopo as informações do incidente clicado.
 	$scope.showIncident = function(event, incident){
 		$scope.currentUserEmail = currentUserService.getUserData().email;
 		$scope.marking = incident;
@@ -24,6 +26,8 @@ angular.module('starter')
 		$scope.modal.show();
 	};
 
+	//Função de transição de páginas. Pode redirecionar para edição de PEV ou
+	//edição de incidentes.
 	$scope.editMarking = function(marking){
 		$scope.modal.hide();
 		console.log(marking);
