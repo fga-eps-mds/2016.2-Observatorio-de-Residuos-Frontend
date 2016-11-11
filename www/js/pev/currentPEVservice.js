99001e4658de94282d1948c0964106e161f712ac
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-11 10:54:00 -0200

    Add comment in currentPevservice

--- www/js/pev/currentPEVservice.js
+++ www/js/pev/currentPEVservice.js
@@ -1,15 +1,16 @@
 angular.module("starter")
 
+//Service para passar a PEV clicada como variável de escopo.
 .service("currentPEVservice", function(){
 	var PEV = {};
 	var getPEV = function(){
 		return PEV;
 	}
-	var setPEV = function(paramPEV){	
+	var setPEV = function(paramPEV){
 		PEV = paramPEV;
 	}
 	return{
-		getPEV: getPEV, 
+		getPEV: getPEV,
 		setPEV: setPEV
 	}
 })
