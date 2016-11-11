8dbd68328be21ff73e07efc7dd43aa220fb9aff9
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-11 10:52:15 -0200

    Add comment in editMarkingCtrl

--- www/js/marking/editMarkingCtrl.js
+++ www/js/marking/editMarkingCtrl.js
@@ -1,15 +1,16 @@
 angular.module("app.controllers")
 
 .controller("editMarkingCtrl", function($scope, $state, currentMarkingService, factoryEditMarking){
+  //Função que atualiza as variáveis de escopo
   $scope.$on("$ionicView.enter", function(event, data){
     $scope.marking = currentMarkingService.getMarking();
     console.log($scope.marking);
   });
 
+  //Função que manda as mudanças para o Backend
   $scope.confirmEditMarking = function(marking){
     console.log(marking);
     factoryEditMarking.save(marking, function(result){
-      console.log("salvou... olhar o terminal do rails...");
       $state.go('tabs.map');
     }, function(erro){
       console.log(error);
