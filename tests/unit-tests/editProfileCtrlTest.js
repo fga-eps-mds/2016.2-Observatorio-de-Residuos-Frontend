4f0b93af0e2baf3cc0c2c8b9417203829fdb2c70
Lucas Amoêdo <lucas_advc@outlook.com>
2016-11-19 15:48:40 -0200

    Test successfull account deactivation

--- tests/unit-tests/editProfileCtrlTest.js
+++ tests/unit-tests/editProfileCtrlTest.js
@@ -1,19 +1,27 @@
-describe('EditProfileCtrl', function(){
+fdescribe('EditProfileCtrl', function(){
   var $controller;
   var $scope;
   var $ionicPopup;
   var $httpBackend;
   var $ionicLoading;
+  var $timeout;
+  var $state;
+  var currentUserService;
   var URL;
   
   beforeEach(module('starter'));
 
   beforeEach(inject(function(_$controller_, _$rootScope_, _$ionicPopup_, 
-                             _$httpBackend_, _$injector_, _$ionicLoading_) {
+                             _$httpBackend_, _$injector_, _$ionicLoading_,
+                             _$timeout_, _$state_, _currentUserService_) {
       $controller = _$controller_;
       $scope = _$rootScope_.$new();
       $ionicPopup = _$ionicPopup_;
       $httpBackend = _$httpBackend_;
+      $ionicLoading = _$ionicLoading_;
+      $timeout = _$timeout_;
+      $state = _$state_;
+      currentUserService = _currentUserService_;
       $httpBackend.when('GET', /\.html$/).respond('');
       URL = _$injector_.get('URL');
   }));
@@ -30,13 +38,34 @@ describe('EditProfileCtrl', function(){
     expect($ionicPopup.show).toHaveBeenCalled();
   });
 
-  it('should successfully deactiavte an account if provided password\
-   is correct', function() {
-    $scope.deactivateAccount(user);
-    $scope.validateDeactivation();
-    $httpBackend.expectGET(URL + '/profiles').respond(200);
-    $httpBackend.expectPOST(URL + '/users/deactivate').respond(200);
-    $httpBackend.flush();
-  })
-  
+  describe('in successfull account deactivation', function() {
+
+    beforeEach(function(){
+      $scope.deactivateAccount(user);
+      $scope.validateDeactivation();
+      $httpBackend.expectGET(URL + '/profiles').respond(200);
+      $httpBackend.expectPOST(URL + '/users/deactivate').respond(200);
+    })
+
+    it('should show a loading message confirming successfull deactivation of\
+     account', function() {
+      spyOn($ionicLoading, 'show');
+      $httpBackend.flush();
+      expect($ionicLoading.show).toHaveBeenCalled();
+    });
+
+    it('should hide the loading message, logoff te user, and redirect to\
+     signing page', function() {
+      spyOn($ionicLoading, 'hide');
+      spyOn($state, 'go');
+      spyOn(currentUserService, 'setUserData');
+      $httpBackend.flush();
+      $timeout.flush();
+      expect($ionicLoading.hide).toHaveBeenCalled();
+      expect(currentUserService.setUserData).toHaveBeenCalledWith(null);
+      expect($state.go).toHaveBeenCalledWith('signin');
+    });
+
+  });
+
 });
