b679511e70f015f19d3e3aa09baa54bca94120a5
Lucas Amoêdo <lucas_advc@outlook.com>
2016-11-19 16:29:26 -0200

    Complete tests for editProfileCtrl.js

--- tests/unit-tests/editProfileCtrlTest.js
+++ tests/unit-tests/editProfileCtrlTest.js
@@ -1,6 +1,7 @@
-fdescribe('EditProfileCtrl', function(){
+describe('EditProfileCtrl', function(){
   var $controller;
   var $scope;
+  var $rootScope;
   var $ionicPopup;
   var $httpBackend;
   var $ionicLoading;
@@ -12,18 +13,19 @@ fdescribe('EditProfileCtrl', function(){
   beforeEach(module('starter'));
 
   beforeEach(inject(function(_$controller_, _$rootScope_, _$ionicPopup_, 
-                             _$httpBackend_, _$injector_, _$ionicLoading_,
-                             _$timeout_, _$state_, _currentUserService_) {
-      $controller = _$controller_;
-      $scope = _$rootScope_.$new();
-      $ionicPopup = _$ionicPopup_;
-      $httpBackend = _$httpBackend_;
-      $ionicLoading = _$ionicLoading_;
-      $timeout = _$timeout_;
-      $state = _$state_;
-      currentUserService = _currentUserService_;
-      $httpBackend.when('GET', /\.html$/).respond('');
-      URL = _$injector_.get('URL');
+   _$httpBackend_, _$injector_, _$ionicLoading_,
+   _$timeout_, _$state_, _currentUserService_) {
+    $controller = _$controller_;
+    $rootScope = _$rootScope_;
+    $scope = _$rootScope_.$new();
+    $ionicPopup = _$ionicPopup_;
+    $httpBackend = _$httpBackend_;
+    $ionicLoading = _$ionicLoading_;
+    $timeout = _$timeout_;
+    $state = _$state_;
+    currentUserService = _currentUserService_;
+    $httpBackend.when('GET', /\.html$/).respond('');
+    URL = _$injector_.get('URL');
   }));
 
   beforeEach(function() {
@@ -31,68 +33,133 @@ fdescribe('EditProfileCtrl', function(){
   });
 
   var user = "some user";
+  var profiles = [{
+    "id_perfil":2,
+    "perfil":"Cidadã(o)",
+    "descricao_perfil":"descricao",
+  },
+  {
+    "id_perfil":3,
+    "perfil":"Síndico(a)/Líder comunitário",
+    "descricao_perfil":"descricao",
+  },
+  {
+    "id_perfil":4,
+    "perfil":"Dirigente de Associação",
+    "descricao_perfil":"descricao",
+  }];
 
-  it('should call a popup when requesting account deactivation', function() {
-    spyOn($ionicPopup, 'show');
-    $scope.deactivateAccount(user);
-    expect($ionicPopup.show).toHaveBeenCalled();
+  describe('when initializing controller', function() {
+
+    it('should get and store all profiles from database', function() {
+      $rootScope.profiles = [];
+      $httpBackend.expectGET(URL + '/profiles').respond(200, profiles);
+      $httpBackend.flush();
+      expect($rootScope.profiles).toEqual(profiles);
+    });
+
+    it('should do nothing on failed connection to database when getting\
+     profiles', function() {
+      $httpBackend.expectGET(URL + '/profiles').respond(400);
+      $httpBackend.flush();
+    });
+
   });
 
-  describe('in successfull account deactivation', function() {
+  describe('when editing an user', function() {
 
     beforeEach(function(){
-      $scope.deactivateAccount(user);
-      $scope.validateDeactivation();
       $httpBackend.expectGET(URL + '/profiles').respond(200);
-      $httpBackend.expectPOST(URL + '/users/deactivate').respond(200);
-    })
-
-    it('should show a loading message confirming successfull deactivation of\
-     account', function() {
-      spyOn($ionicLoading, 'show');
-      $httpBackend.flush();
-      expect($ionicLoading.show).toHaveBeenCalled();
-    });
-
-    it('should hide the loading message, logoff te user, and redirect to\
-     signing page', function() {
-      spyOn($ionicLoading, 'hide');
-      spyOn($state, 'go');
       spyOn(currentUserService, 'setUserData');
-      $httpBackend.flush();
-      $timeout.flush();
-      expect($ionicLoading.hide).toHaveBeenCalled();
-      expect(currentUserService.setUserData).toHaveBeenCalledWith(null);
-      expect($state.go).toHaveBeenCalledWith('signin');
+      spyOn($state, 'go');
+      $scope.editUser(user);
+
     });
+
+    it('should update the current user and redirect to profile on successfull\
+     edit', function() {
+      var editedUser = user;
+      $httpBackend.expectPOST(URL + '/users/edit').respond(200, editedUser);
+      $httpBackend.flush();
+      expect(currentUserService.setUserData).toHaveBeenCalledWith(editedUser);
+      expect($state.go).toHaveBeenCalledWith('tabs.profile');
+    });
+
+    it('should do nothing on failed connection when editing an\
+     user', function() {
+      $httpBackend.expectPOST(URL + '/users/edit').respond(400);
+      $httpBackend.flush();
+      expect(currentUserService.setUserData).not.toHaveBeenCalled();
+      expect($state.go).not.toHaveBeenCalled();
+    })
 
   });
 
-  describe('in failed account deactivation', function() {
-
-    beforeEach(function() {
-      $httpBackend.expectGET(URL + '/profiles').respond(200);
+  describe('when trying to deactivate an account', function() {
+    it('should call a popup when requesting account deactivation', function() {
+      spyOn($ionicPopup, 'show');
       $scope.deactivateAccount(user);
-      $scope.validateDeactivation();
-      spyOn($ionicPopup, 'alert');
+      expect($ionicPopup.show).toHaveBeenCalled();
     });
 
-    it('should show a popup alert containing the error code', function() {
-      $httpBackend.expectPOST(URL + '/users/deactivate').respond(400);
-      $httpBackend.flush();
-      expect($ionicPopup.alert).toHaveBeenCalledWith({
-        title: 'Erro', template: 'Código do erro: 400.'
+    describe('in successfull account deactivation', function() {
+
+      beforeEach(function(){
+        $scope.deactivateAccount(user);
+        $scope.validateDeactivation();
+        $httpBackend.expectGET(URL + '/profiles').respond(200);
+        $httpBackend.expectPOST(URL + '/users/deactivate').respond(200);
+      })
+
+      it('should show a loading message confirming successfull deactivation of\
+       account', function() {
+        spyOn($ionicLoading, 'show');
+        $httpBackend.flush();
+        expect($ionicLoading.show).toHaveBeenCalled();
       });
+
+      it('should hide the loading message, logoff te user, and redirect to\
+       signing page', function() {
+        spyOn($ionicLoading, 'hide');
+        spyOn($state, 'go');
+        spyOn(currentUserService, 'setUserData');
+        $httpBackend.flush();
+        $timeout.flush();
+        expect($ionicLoading.hide).toHaveBeenCalled();
+        expect(currentUserService.setUserData).toHaveBeenCalledWith(null);
+        expect($state.go).toHaveBeenCalledWith('signin');
+      });
+
     });
 
-    it('should show a popup alert contaning wrong password message  if password\
-     was wrong', function() {
-      $httpBackend.expectPOST(URL + '/users/deactivate').respond(401);
-      $httpBackend.flush();
-      expect($ionicPopup.alert).toHaveBeenCalledWith({
-        title: 'Erro', template: 'Senha incorreta.'
+    describe('in failed account deactivation', function() {
+
+      beforeEach(function() {
+        $httpBackend.expectGET(URL + '/profiles').respond(200);
+        $scope.deactivateAccount(user);
+        $scope.validateDeactivation();
+        spyOn($ionicPopup, 'alert');
       });
+
+      it('should show a popup alert containing the error code', function() {
+        $httpBackend.expectPOST(URL + '/users/deactivate').respond(400);
+        $httpBackend.flush();
+        expect($ionicPopup.alert).toHaveBeenCalledWith({
+          title: 'Erro', template: 'Código do erro: 400.'
+        });
+      });
+
+      it('should show a popup alert contaning wrong password message  if password\
+       was wrong', function() {
+        $httpBackend.expectPOST(URL + '/users/deactivate').respond(401);
+        $httpBackend.flush();
+        expect($ionicPopup.alert).toHaveBeenCalledWith({
+          title: 'Erro', template: 'Senha incorreta.'
+        });
+      });
+
     });
+
   });
 
 });
