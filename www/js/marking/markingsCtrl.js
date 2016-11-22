f5d2ce9c615677cd88f90f64e4a6330b51898b2e
Jefersonalves <jefersonalvesf2009@gmail.com>
2016-11-22 21:52:22 -0200

    Add nearby markings functionality

--- www/js/marking/markingsCtrl.js
+++ www/js/marking/markingsCtrl.js
@@ -1,40 +1,52 @@
 angular.module('app.controllers')
 
 //Controller responsible for markings in to contribuindo
-.controller('markingsCtrl', function($scope, $http, URL, $rootScope, currentUserService, markingService, $state) {
+.controller('markingsCtrl', function($scope, $http, URL, $rootScope, currentUserService, markingService, $state, NgMap) {
   $rootScope.markings = [];
   $rootScope.marking_types = [];
+  $rootScope.nearbyMarkings = [];
   $scope.currentUserEmail = currentUserService.getUserData().email;
+//==========================================================
+  NgMap.getGeoLocation().then(function(map) {
+    latitudeAtual = map.lat();
+    longitudeAtual = map.lng();
 
-
-  $http.get(URL + '/marking_types')
-  .success(function(content){
-    angular.forEach(content, function(value, key) {
-      $rootScope.marking_types.push(value);
-    })
-    $http.get(URL + '/markings')
+    var raio = 15; //distance in meter
+//==========================================================
+    $http.get(URL + '/marking_types')
     .success(function(content){
       angular.forEach(content, function(value, key) {
-        angular.forEach($scope.marking_types, function (otherValue) {
-          if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
-            value.tipo_incidente = otherValue.tipo_incidente;
+        $rootScope.marking_types.push(value);
+      });
+      $http.get(URL + '/markings')
+      .success(function(content){
+        console.log(content)
+        angular.forEach(content, function(value, key) {
+          $rootScope.markings.push(value);
+//==========================================================
+          if(getDistance(latitudeAtual, longitudeAtual, value.latitude, value.longitude) <= raio){
+              $rootScope.nearbyMarkings.push(value);
+              console.log("huehuehue");
           }
-        })
-        $rootScope.markings.push(value);
+          console.log(value.titulo_incidente);
+          console.log(getDistance(latitudeAtual, longitudeAtual, value.latitude, value.longitude));
+//==========================================================          
+          angular.forEach($scope.marking_types, function (otherValue) {
+            if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
+              value.tipo_incidente = otherValue.tipo_incidente;
+            }
+          });
+        });
       })
+      .error(function(error){
+        console.log("Error");
+      });
     })
     .error(function(error){
       console.log("Error");
-    })
-  })
-  .error(function(error){
-    console.log("Error");
-  })
+    });
+});
 
-  $scope.openMarking = function (marking) {
-    markingService.setMarking(marking);
-    $state.go("tabs.markings.detailMarking");
-  }
 //================================================================
   var rad = function(x) {
     return x * Math.PI / 180;
