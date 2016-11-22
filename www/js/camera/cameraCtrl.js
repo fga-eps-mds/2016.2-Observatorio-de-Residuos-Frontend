c6e111211bd41208c0e6054fbbe19cfa7ce2fc90
caue96 <cauemateus@hotmail.com>
2016-11-21 23:30:34 -0200

    Modify all camera controller

--- www/js/camera/cameraCtrl.js
+++ www/js/camera/cameraCtrl.js
@@ -1,27 +1,77 @@
-angular.module('app.controllers')
+angular.module('starter')
 
-  .controller("cameraCtrl", function ($scope, $cordovaCamera) {
-    /* That is the camera's controller, the following methods are to take a picture or choose one from gallery*/
+    .controller('cameraCtrl', function($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, $cordovaEmailComposer, $ionicActionSheet, ImageService, FileService) {
 
-  $scope.imgURI = "http://placehold.it/800x600";
+      $ionicPlatform.ready(function() {
+        $scope.images = FileService.images();
+      });
 
-  $scope.takePhoto = function (srcType) {
-    var options = {
-      quality: 75,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: (srcType=="CAMERA")?Camera.PictureSourceType.CAMERA:Camera.PictureSourceType.PHOTOLIBRARY,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 800,
-      targetHeight: 600,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false
-    };
+      $scope.urlForImage = function(imageName) {
+        var trueOrigin = cordova.file.dataDirectory + imageName;
+        return trueOrigin;
+      }
 
-    $cordovaCamera.getPicture(options).then(function (imageData) {
-      $scope.imgURI = "data:image/jpeg;base64," + imageData;
-    }, function (err) {
-      console.log("Erro Camera - Verificar o erro encontrado");
+      $scope.addMedia = function() {
+        $scope.hideSheet = $ionicActionSheet.show({
+          buttons: [
+            { text: 'Tirar foto' },
+            { text: 'Pegar imagem' }
+          ],
+          titleText: 'Adicionar imagem',
+          cancelText: 'Cancelar',
+          buttonClicked: function(index) {
+            $scope.addImage(index);
+          }
+        });
+      }
+
+      $scope.addImage = function(type) {
+        $scope.hideSheet();
+        ImageService.handleMediaDialog(type).then(function() {
+          $scope.$apply();
+        });
+      }
+
+      $scope.sendEmail = function() {
+        if ($scope.images != null && $scope.images.length > 0) {
+          var mailImages = [];
+          var savedImages = $scope.images;
+          if ($cordovaDevice.getPlatform() == 'Android') {
+            // Currently only working for one image..
+            var imageUrl = $scope.urlForImage(savedImages[0]);
+            var name = imageUrl.substr(imageUrl.lastIndexOf('/') + 1);
+            var namePath = imageUrl.substr(0, imageUrl.lastIndexOf('/') + 1);
+            $cordovaFile.copyFile(namePath, name, cordova.file.externalRootDirectory, name)
+                .then(function(info) {
+                  mailImages.push('' + cordova.file.externalRootDirectory + name);
+                  $scope.openMailComposer(mailImages);
+                }, function(e) {
+                  reject();
+                });
+          } else {
+            for (var i = 0; i < savedImages.length; i++) {
+              mailImages.push('' + $scope.urlForImage(savedImages[i]));
+            }
+            $scope.openMailComposer(mailImages);
+          }
+        }
+      }
+
+      $scope.openMailComposer = function(attachments) {
+        var bodyText = '<html><h2>My Images</h2></html>';
+        var email = {
+          to: 'some@email.com',
+          attachments: attachments,
+          subject: 'Devdactic Images',
+          body: bodyText,
+          isHtml: true
+        };
+
+        $cordovaEmailComposer.open(email).then(null, function() {
+          for (var i = 0; i < attachments.length; i++) {
+            var name = attachments[i].substr(attachments[i].lastIndexOf('/') + 1);
+            $cordovaFile.removeFile(cordova.file.externalRootDirectory, name);
+          }
+        });
+      }
     });
-  };
-});
