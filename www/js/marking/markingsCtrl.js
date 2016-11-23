c17914f36cad5bdb37cf2034cab5cdef55127bd2
Flavio Costa <Skingk.f@gmail.com>
2016-11-22 23:40:43 -0200

    Create loadings to marking controller

--- www/js/marking/markingsCtrl.js
+++ www/js/marking/markingsCtrl.js
@@ -1,19 +1,23 @@
 angular.module('app.controllers')
 
 //Controller responsible for markings in to contribuindo
-.controller('markingsCtrl', function($scope, $http, URL, $rootScope, currentUserService, markingService, $state) {
+.controller('markingsCtrl', function($scope, $http, URL, $rootScope, currentUserService, markingService, $state, $ionicLoading) {
   $rootScope.markings = [];
   $rootScope.marking_types = [];
   $scope.currentUserEmail = currentUserService.getUserData().email;
 
-
+  $ionicLoading.show({
+    template: 'Por favor, aguarde... <ion-spinner icon="android"></ion-spinner>'
+  });
   $http.get(URL + '/marking_types')
   .success(function(content){
+    $ionicLoading.hide();
     angular.forEach(content, function(value, key) {
       $rootScope.marking_types.push(value);
     })
     $http.get(URL + '/markings')
     .success(function(content){
+      $ionicLoading.hide();
       angular.forEach(content, function(value, key) {
         angular.forEach($scope.marking_types, function (otherValue) {
           if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
@@ -24,10 +28,12 @@ angular.module('app.controllers')
       })
     })
     .error(function(error){
+      $ionicLoading.hide();
       console.log("Error");
     })
   })
   .error(function(error){
+    $ionicLoading.hide();
     console.log("Error");
   })
 
@@ -35,6 +41,4 @@ angular.module('app.controllers')
     markingService.setMarking(marking);
     $state.go("tabs.markings.detailMarking");
   }
-
-
 })
