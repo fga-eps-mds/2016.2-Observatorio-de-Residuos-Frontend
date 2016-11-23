9fb1d39eb472844516c25625a7e3dda8f5ad4622
Flavio Costa <Skingk.f@gmail.com>
2016-11-22 23:40:14 -0200

    Create loadings to pev controller

--- www/js/pev/newPevCtrl.js
+++ www/js/pev/newPevCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers')
 
-.controller("newPevCtrl", function ($ionicHistory, NgMap,$state, $scope, $rootScope,currentUserService, factoryPEV, $ionicPopup, URL) {
+.controller("newPevCtrl", function ($ionicHistory, NgMap,$state, $scope, $rootScope,currentUserService, factoryPEV, $ionicPopup, URL, $ionicLoading) {
   var options = {enableHighAccuracy: true};
   if(angular.isUndefined($rootScope.pevs)) {
     $rootScope.pevs = [];
@@ -14,10 +14,15 @@ angular.module('app.controllers')
         pev.author_email = currentUserService.getUserData().email;
         pev.total_confirmacoes_funcionando = 0;
         pev.total_confirmacoes_fechou = 0;
+
         factoryPEV.save(pev, function (result){
+          $ionicLoading.show({
+          template: 'Por favor, aguarde... <ion-spinner icon="android"></ion-spinner>'
+          });
           result.author_email = pev.author_email;
           result.author_name = pev.author_name;
           $rootScope.pevs.push(result);
+          $ionicLoading.hide();
           var alertPopup = $ionicPopup.alert({
             title: 'PEV cadastrada com sucesso',
             template: 'Obrigado por contribuir!'
@@ -29,6 +34,7 @@ angular.module('app.controllers')
           $state.go('tabs.map')
           /* This state must be reset and the back button too */
         }, function (error) {
+          $ionicLoading.hide();
           var alertPopup = $ionicPopup.alert({
             title: 'Informações insuficientes',
             template: 'Preencha as informações corretamente!'
--- www/js/pev/pevsCtrl.js
+++ www/js/pev/pevsCtrl.js
@@ -1,19 +1,22 @@
 angular.module('app.controllers')
 
 //Controller responsible for pevs in to contribuindo
-    .controller('pevsCtrl', function($scope, $http, URL, $rootScope, currentUserService) {
+    .controller('pevsCtrl', function($scope, $http, URL, $rootScope, currentUserService, $ionicLoading) {
         $rootScope.pevs = [];
         $rootScope.pev_types = [];
         $scope.currentUserEmail = currentUserService.getUserData().email;
-
-
+          $ionicLoading.show({
+          template: 'Por favor, aguarde... <ion-spinner icon="android"></ion-spinner>'
+          });
         $http.get(URL + '/pev_types')
             .success(function(content){
+                $ionicLoading.hide();
                 angular.forEach(content, function(value, key) {
                     $rootScope.pev_types.push(value);
                 })
                 $http.get(URL + '/pevs')
                     .success(function(content){
+                        $ionicLoading.hide();
                         angular.forEach(content, function(value, key) {
                             angular.forEach($scope.pev_types, function (otherValue) {
                                 if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
@@ -24,10 +27,12 @@ angular.module('app.controllers')
                         })
                     })
                     .error(function(error){
+                        $ionicLoading.hide();
                         console.log("Error");
                     })
             })
             .error(function(error){
+                $ionicLoading.hide();
                 console.log("Error");
             })
 
