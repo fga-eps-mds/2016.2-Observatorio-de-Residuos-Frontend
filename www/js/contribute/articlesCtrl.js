1d23d1c008e53195010655e2dac71e801efaba96
Flavio Costa <Skingk.f@gmail.com>
2016-11-22 23:41:47 -0200

    Create loading to contribute controller

--- www/js/contribute/articlesCtrl.js
+++ www/js/contribute/articlesCtrl.js
@@ -1,15 +1,20 @@
 angular.module('app.controllers')
 
     //Controller responsible for articles in to contribuindo
-    .controller('articlesCtrl', function($scope, $http, URL, $rootScope, articleService, $state) {
+    .controller('articlesCtrl', function($scope, $http, URL, $rootScope, articleService, $state, $ionicLoading) {
         $rootScope.articles = [];
+        $ionicLoading.show({
+            template: 'Por favor, aguarde... <ion-spinner icon="android"></ion-spinner>'
+        });
         $http.get(URL + '/articles')
             .success(function(content){
+                $ionicLoading.hide();
                 angular.forEach(content, function(value, key) {
                     $rootScope.articles.push(value);
                 })
             })
             .error(function(error){
+                $ionicLoading.hide();
                 console.log("Error");
             })
 
