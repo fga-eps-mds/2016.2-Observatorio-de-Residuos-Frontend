68f3f171b487878e0ad4e0512fb8d10f2acd961a
Jefersonalves <jefersonalvesf2009@gmail.com>
2016-11-24 00:50:56 -0200

    Add pevsCtrl to myPevs.html

--- www/js/pev/pevsCtrl.js
+++ www/js/pev/pevsCtrl.js
@@ -4,7 +4,6 @@ angular.module('app.controllers')
 //Controller responsible for pevs in to contribuindo
 .controller('pevsCtrl', function($scope, $http, URL, $rootScope, currentUserService, NgMap, distanceMarkingService) {
     $rootScope.pevs = [];
-    $rootScope.pev_types = [];
     $rootScope.nearbyPevs = [];
     $scope.currentUserEmail = currentUserService.getUserData().email;
 
@@ -12,40 +11,42 @@ angular.module('app.controllers')
         currentLatitude = map.lat();
         currentLongitude = map.lng();
 
-        var raio = 30; //distance in meter
+        var raio = 3; //distance in meter
 
-        $http.get(URL + '/pev_types')
+        $http.get(URL + '/pevs')
         .success(function(content){
             angular.forEach(content, function(value, key) {
-                $rootScope.pev_types.push(value);
+                $rootScope.pev_types = "";
+                if (value.paper == true)
+                    $rootScope.pev_types = "Papel";
+                if (value.glass == true)
+                    $rootScope.pev_types += " Vidro";
+                if (value.metal == true)
+                    $rootScope.pev_types += " Metal";
+                if (value.plastic == true)
+                    $rootScope.pev_types += " Plástico";
+
+                console.log($rootScope.pev_types);
+                $rootScope.pevs.push(value);
+                if(distanceMarkingService.getDistance(currentLatitude, currentLongitude, value.latitude, value.longitude) <= raio){
+                  $rootScope.nearbyPevs.push(value);
+                }
+                console.log(value.titulo_pev);
+                console.log(distanceMarkingService.getDistance(currentLatitude, currentLongitude, value.latitude, value.longitude));       
+                //angular.forEach($scope.pev_types, function (otherValue) {
+                //if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
+                //    value.tipo_incidente = otherValue.tipo_incidente;
+                //}
             })
-            $http.get(URL + '/pevs')
-            .success(function(content){
-                angular.forEach(content, function(value, key) {
-                    $rootScope.pevs.push(value);
-                    if(distanceMarkingService.getDistance(currentLatitude, currentLongitude, value.latitude, value.longitude) <= raio){
-                          $rootScope.nearbyPevs.push(value);
-                    }
-                    console.log(value.titulo_incidente);
-                    console.log(distanceMarkingService.getDistance(currentLatitude, currentLongitude, value.latitude, value.longitude));       
-                    angular.forEach($scope.pev_types, function (otherValue) {
-                        if (otherValue.id_tipo_incidente == value.id_tipo_incidente){
-                            value.tipo_incidente = otherValue.tipo_incidente;
-                        }
-                    })
-                })
-            })
-            .error(function(error){
-                console.log("Error");
-            });
         })
         .error(function(error){
             console.log("Error");
         });
-
-        $scope.openPev = function (pev) {
-            pevService.setPev(pev);
-            $state.go("tabs.to-contribuindo.detailPev");
-        }
     });
+
+    $scope.openPev = function (pev) {
+        pevService.setPev(pev);
+        $state.go("tabs.to-contribuindo.detailPev");
+    }
+
 });
