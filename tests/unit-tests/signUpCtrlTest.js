e89dc384084bf73a16ec7a67afc7a06dce6dab2d
pablodiegoss <pablodiegoss@hotmail.com>
2016-11-23 22:27:49 -0200

    improve SignUpTests

--- tests/unit-tests/signUpCtrlTest.js
+++ tests/unit-tests/signUpCtrlTest.js
@@ -1,6 +1,7 @@
 describe('signupCtrl', function() {
   var $controller;
   var $scope = {};
+  var $rootScope;
   var currentUserService;
   var factoryRegister;
   var $httpBackend;
@@ -9,30 +10,52 @@ describe('signupCtrl', function() {
 
   beforeEach(module('starter'));
 
-  beforeEach(inject(function(_$controller_, _currentUserService_,
-                             _factoryRegister_, _$httpBackend_, _$state_,
-                             $injector){
+  beforeEach(inject(function(_$rootScope_,_$controller_, _currentUserService_,_factoryRegister_, _$httpBackend_, _$state_,$injector){
     $controller = _$controller_;
     currentUserService = _currentUserService_;
     factoryRegister = _factoryRegister_;
     $state = _$state_;
+    $rootScope = _$rootScope_
     $httpBackend = _$httpBackend_;
     $httpBackend.when('GET', /\.html$/).respond('');
     URL = $injector.get('URL');
   }));
 
-  beforeEach(function() {
-    var controller = $controller('signupCtrl',
-                                  {$scope: $scope,
-                                  currentUserService: currentUserService
-                                  });
+  var profiles = [{
+    "id_perfil":2,
+    "perfil":"Cidadã(o)",
+    "descricao_perfil":"descricao",
+  },
+  {
+    "id_perfil":3,
+    "perfil":"Síndico(a)/Líder comunitário",
+    "descricao_perfil":"descricao",
+  },
+  {
+    "id_perfil":4,
+    "perfil":"Dirigente de Associação",
+    "descricao_perfil":"descricao",
+  }];
+  it('should get and store all profiles from database', function() {
+    $rootScope.profiles = [];
+    $httpBackend.expectGET(URL + '/profiles').respond(200, profiles);
+    var controller = $controller('signupCtrl',{$scope: $scope,currentUserService: currentUserService});
+    $httpBackend.flush();
+    expect($rootScope.profiles).toEqual(profiles);
   });
-
   it('should get user data from the current user', function() {
-    expect($scope.user).not.toBe(null);
+    currentUserService.setUserData("Amoedo");
+    var controller = $controller('signupCtrl',{$scope: $scope,currentUserService: currentUserService});
+    expect($scope.user).toEqual(currentUserService.getUserData());
   });
-
+  it('should create a new JSON in Scope if current user is undefined', function() {
+      var controller = $controller('signupCtrl',{$scope: $scope,currentUserService: currentUserService});
+      expect($scope.user).toEqual({});
+  });
   describe('user validation', function() {
+    beforeEach(function() {
+      var controller = $controller('signupCtrl',{$scope: $scope,currentUserService: currentUserService});
+    });
     var user = {name: "Amoêdo", email: "amoedo@email.com",
               password_digest: "cacofonia", password_confirmation: "cacofonia"};
 
@@ -40,7 +63,6 @@ describe('signupCtrl', function() {
       $scope.registerEmail(user);
       expect(user.password_digest).not.toEqual('cacofonia');
     });
-
     it('should set invalidEmail to false and redirect to tabs.map when getting\
        a successfull response from server during email validation', function() {
       $httpBackend.when('GET', URL+"/profiles").respond(200);
@@ -52,7 +74,6 @@ describe('signupCtrl', function() {
       expect($scope.emailAlreadyUsed).toBeFalsy();
       expect($state.go).toHaveBeenCalledWith('tabs.home');//------
     });
-
     it('should set invalidEmail to true when getting an error response 400 from\
      server during email validation', function() {
       $httpBackend.when('GET', URL+"/profiles").respond(200);
@@ -71,5 +92,11 @@ describe('signupCtrl', function() {
        expect($scope.invalidEmail).toBeFalsy();
        expect($scope.emailAlreadyUsed).toBeTruthy();
      })
+
+     it('should leave profiles empty if failed to load', function() {
+       $httpBackend.when('GET', URL+"/profiles").respond(400);
+       $httpBackend.flush();
+       expect($rootScope.profiles).toEqual([]);
+      });
   });
 });
