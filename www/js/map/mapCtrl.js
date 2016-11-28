c7944c53dffe0148880a97d4b90bbcc45b3dcdc7
Lucas Amoêdo <lucas_advc@outlook.com>
2016-11-28 21:25:32 -0200

    Add deviceready listener for geolocation at mapCtrl.js

--- www/js/map/mapCtrl.js
+++ www/js/map/mapCtrl.js
@@ -9,58 +9,68 @@ angular.module('app.controllers')
       template: 'Carregando mapa, aguarde... <ion-spinner icon="android"></ion-spinner>'
     });
 
-    NgMap.getGeoLocation().then(function(map) {
-      $ionicLoading.hide();
-      $scope.currentLocation ="["+ map.lat()+","+map.lng()+"]"
-    }, function(error) {
-      $ionicLoading.hide( );
-      $ionicPopup.alert({template: "Não foi possível encontrar o GPS."})
-      .then(function() {
-        $state.go('tabs.home');
+    document.addEventListener('deviceready', function() {
+
+      NgMap.getGeoLocation().then(function(map) {
+      
+        $ionicLoading.hide();
+        $scope.currentLocation ="["+ map.lat()+","+map.lng()+"]"
+      
+      }, function(error) {
+
+        $ionicLoading.hide( );
+        $ionicPopup.alert({template: "Não foi possível encontrar o GPS."})
+        .then(function() {
+          $state.go('tabs.home');
+        });
+      
       });
+    
     });
 
   });
   
   NgMap.getMap().then(function(map) {
-    if(angular.isUndefined($rootScope.markings)){
-      $rootScope.markings = [];
-    }
-    if(angular.isUndefined($rootScope.pevs)){
-      $rootScope.pevs = [];
-    }
-        //Initialize all PEVs saved in database
-        $http.get(URL + '/pevs')
-        .success(function(content){
+    google.maps.event.addListener(map, "idle", function(){
+      google.maps.event.trigger(map, 'resize'); 
+    });
+  })
 
-          angular.forEach(content, function(value, key) {
-            $rootScope.pevs.push(value);
-          })
-        })
-        .error(function(data){
+  if(angular.isUndefined($rootScope.markings)){
+    $rootScope.markings = [];
+  }
+  if(angular.isUndefined($rootScope.pevs)){
+    $rootScope.pevs = [];
+  }
+  $http.get(URL + '/pevs')
+  .success(function(content){
 
-          $ionicPopup.alert({
-            template: 'Erro ao carregar o mapa com as PEVs.',
-            title: 'Erro'
-          });
-          console.log(data)
-        });
+    angular.forEach(content, function(value, key) {
+      $rootScope.pevs.push(value);
+    })
+  })
+  .error(function(data){
 
-        //Initialize all Markings saved in database
-        $http.get(URL + '/markings')
-        .success(function(content){
+    $ionicPopup.alert({
+      template: 'Erro ao carregar o mapa com as PEVs.',
+      title: 'Erro'
+    });
+    console.log(data)
+  });
 
-          angular.forEach(content, function(value, key) {
-            $rootScope.markings.push(value);
-          })
-        })
-        .error(function(data){
+  $http.get(URL + '/markings')
+  .success(function(content){
 
-          $ionicPopup.alert({
-            template: 'Erro ao carregar o mapa com os Incidentes.',
-            title: 'Erro'
-          });
-          console.log(data)
-        });
-      })
+    angular.forEach(content, function(value, key) {
+      $rootScope.markings.push(value);
+    })
+  })
+  .error(function(data){
+
+    $ionicPopup.alert({
+      template: 'Erro ao carregar o mapa com os Incidentes.',
+      title: 'Erro'
+    });
+    console.log(data)
+  });
 })
