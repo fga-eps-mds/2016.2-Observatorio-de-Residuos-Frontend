d13370b96700a3e07157174a220088fad6baea83
Lucas Amoêdo <lucas_advc@outlook.com>
2016-12-02 15:23:59 -0200

    Redo profile page

--- www/js/user/profile/profileCtrl.js
+++ www/js/user/profile/profileCtrl.js
@@ -1,7 +1,15 @@
 angular.module('app.controllers')
 
-    .controller('profileCtrl', function(currentUserService, $scope){
-        //Function that get current user data.
-        $scope.user = currentUserService.getUserData()
-    })
+.controller('profileCtrl', function(currentUserService, $scope, $http, URL, $ionicPopup){
+  $scope.user = currentUserService.getUserData();
+
+  $http.get(URL + '/profiles/' + $scope.user.perfil.toLowerCase())
+  .success(function(profile) {
+    $scope.profile = profile;
+    document.getElementById('profile-description').innerHTML = profile.descricao_perfil;
+  })
+  .error(function(error) {
+    $ionicPopup.alert({template: 'Falha ao obter descrição do perfil'});
+  })
+});
 
