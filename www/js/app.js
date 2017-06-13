d703a57ea9b43ff368a95e0920272bf8bdf087b2
Lucas Amoêdo <lucas_advc@outlook.com>
2017-06-13 14:39:59 -0300

    Add exit confirmation

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,24 @@
 angular.module('starter', ['ionic', 'ngResource','app.controllers', 'ngMap', 'ngCordova'])
 
-.run(function($ionicPlatform, currentUserService, $state, loginService) {
+.run(function($ionicPlatform, currentUserService, $state, loginService,
+              $rootScope, $ionicHistory) {
+
+  $ionicPlatform.registerBackButtonAction(function(e) {
+    if ($rootScope.backButtonPressedToExit) {
+      ionic.Platform.exitApp();
+    } else if ($ionicHistory.backView()) {
+      $ionicHistory.goBack();
+    } else {
+      $rootScope.backButtonPressedToExit = true;
+      window.plugins.toast.showShortBottom("Pressione novamente para sair");
+      setTimeout(function(){
+        $rootScope.backButtonPressedToExit = false;
+      }, 2000);
+    }
+    e.preventDefault();
+
+    return false;
+  }, 101);
 
   $ionicPlatform.ready(function() {
     var user = window.localStorage.getItem('autoLoginUser');
